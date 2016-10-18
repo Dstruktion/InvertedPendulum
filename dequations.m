e1c8aae126cd540c327f64ec174c791050845d68
Dstruktion <struk.daniel@gmail.com>
2016-10-18 16:52:50 -0400

    Update dequations.m

--- dequations.m
+++ dequations.m
@@ -16,9 +16,9 @@ switch Mode
     case 'Force'
         F = u;
         qdot(1,1) = w;
-        qdot(2,1) = (F*ct+(ct^2-mt/mp)*b*w+mt*g*st-mp*w^2*L*st*ct)/(mt*L-mp*L*ct^2);
+        qdot(2,1) = 0;
         qdot(3,1) = v;
-        qdot(4,1) = (F+mp*g*ct*st-mp*w^2*L*st)/(mt-mp*ct^2);     
+        qdot(4,1) = 0;     
 
     case 'Velocity' %these equations are actually for acceleration control, but the mode
         %for the pendulum class says velocity, and we just differentiate.
